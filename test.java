0dc643daf432a9918c7bcc1820ab2d7d5ede0363
AnisLen <32750426+AnisLen@users.noreply.github.com>
2017-10-14 00:15:36 +0100

    Add test file

---
+++ test.java
@@ -0,0 +1,55 @@
+
+import org.junit.Test ;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by anis on 12/10/17.
+ */
+
+public class MainTest {
+
+    @Test
+    public void TestTrie(){
+
+    }
+
+    @Test
+    public void TestSomme(){
+
+    }
+
+    @Test
+    public void TestMaxMin(){
+
+    }
+
+    @Test
+    public void TestInverser(){
+        VectorHelper m = new VectorHelper() ;
+
+        int a [] = {1,2,3,4,5,6,7} ;
+        int b [] = {7,6,5,4,3,2,1} ;
+        assertArrayEquals(b, m.inverser(a));
+
+return;
+    }
+
+    @Test
+    public void TestFourmule() {
+
+    }
+
+    @Test
+    public void TestApp()  {
+    }
+
+    @Test
+    public void TestMain()  {
+    }
+
+
+
+}
